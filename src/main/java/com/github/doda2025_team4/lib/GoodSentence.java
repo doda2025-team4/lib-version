898fed1d6a45955be95b0fbb9c38eb3714f73bdd
Kasper van Maasdam <k.vanmaasdam@student.tudelft.nl>
2025-11-15 12:07:48 +0100

    feat: sentence generator

---
+++ src/main/java/com/github/doda2025_team4/lib/GoodSentence.java
@@ -0,0 +1,30 @@
+package com.github.doda2025_team4.lib;
+
+import java.util.Random;
+
+
+public class GoodSentence
+{
+    private final String[] sentences = {
+        "Team 4 is the best!",
+        "Team 4 knows how to do it.",
+        "You wish you were a member of team 4...",
+        "Programming is easy for team 4.",
+        "Team 4 can do these assignments in its sleep.",
+        "Team 4 will take care of you. Team 4 is kind."
+    };
+
+    private final Random randomGenerator;
+
+    public GoodSentence() {
+        this(new Random());
+    }
+
+    GoodSentence(Random randomGenerator) {
+        this.randomGenerator = randomGenerator;
+    }
+
+    public String generateSentence() {
+        return sentences[this.randomGenerator.nextInt(this.sentences.length)];
+    }
+}
